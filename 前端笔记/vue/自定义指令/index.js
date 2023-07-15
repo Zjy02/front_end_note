2a6ee9777f5f72e8972fe69e3186a245ec85d368
zjy <1831783781@qq.com>
2023-07-15 16:34:55 +0800

    new

---
+++ 前端笔记/vue/自定义指令/index.js
@@ -0,0 +1,12 @@
+//将所有地指令导入，导出
+import Focus from "./focus";
+
+export default (app)=>{
+    Focus(app)
+}
+//在main.js中
+// import App from './App.vue'
+// const App = createApp(App)
+// import directive from '路径'
+// directive(app)
+// 然后就可以直接在组件中使用v-name,name为自己定义的名字
